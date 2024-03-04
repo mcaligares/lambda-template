59aa8f5b758887956ee971b21e274a33077988e2
Miguel Augusto Caligares <mcaligares@gmail.com>
2024-03-04 00:55:43 -0300

    update env file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 app.get("/", (req, res, next) => {
   return res.status(200).json({
     message: "Hello from root!",
+    DEBUG: process.env.DEBUG,
     DATABASE_URL: process.env.DATABASE_URL ? process.env.DATABASE_URL : 'not here',
   });
 });
