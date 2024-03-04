69d34878684117e5f3721c3ab1d5e97695d860ee
Miguel Augusto Caligares <mcaligares@gmail.com>
2024-03-04 15:26:48 -0300

    add migration and generate scripts

---
+++ src/db/schemas.js
@@ -0,0 +1,10 @@
+const { text, pgTable, timestamp, serial } = require("drizzle-orm/pg-core");
+
+const LeadTable = pgTable('leads', {
+  id: serial('id').primaryKey().notNull(),
+  email: text('email').notNull(),
+  description: text('description').default('This is a default value'),
+  createdAt: timestamp('created_at').defaultNow(),
+});
+
+module.exports.LeadTable = LeadTable;
