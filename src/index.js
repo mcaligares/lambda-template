e714d4ec6d185d36e064faac56ce12a62c75e7ca
Miguel Augusto Caligares <mcaligares@gmail.com>
2024-03-04 00:39:05 -0300

    add env config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 app.get("/", (req, res, next) => {
   return res.status(200).json({
     message: "Hello from root!",
+    DATABASE_URL: process.env.DATABASE_URL ? process.env.DATABASE_URL : 'not here',
   });
 });
 
@@ -18,6 +19,10 @@ app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
   });
+});
+
+app.listen(3000, () => {
+  console.log('running at localhost:3000')
 });
 
 module.exports.handler = serverless(app);
